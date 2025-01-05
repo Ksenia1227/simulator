4eb6adba6c4180c9cd156802c6c1388c65f38c88
ShutovaAnna <67040@pspu.ru>
2025-01-06 03:33:17 +0500

    door and textures

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,19 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
-{
+
+    [SerializeField] string tagFilter;
+
     [SerializeField] UnityEvent onTriggerEnter;
     [SerializeField] UnityEvent onTriggerExit;
+
     void OnTriggerEnter(Collider other)
     {
-        
+        if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerEnter.Invoke();
     }
 
     void OnTriggerExit(Collider other)
     {
-        
+        if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerExit.Invoke();
     }
 }
